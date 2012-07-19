55eb3405babb56fdbbe16d57b3b9a383b6573bf5
Raphaël Pinson <raphink@gmail.com>
2012-07-20 01:28:53 +0200

    Use lipsum in test file, check for exact text on page 3

--- spec/pdf_spec.rb
+++ spec/pdf_spec.rb
@@ -21,19 +21,19 @@ end
 
 describe 'Nowidow-test PDF' do
   reader = PDF::Reader.new('nowidow-test.pdf')
-  it 'should have 8 pages' do
-    reader.page_count.should eq(16)
+  it 'should have 3 pages' do
+    reader.page_count.should eq(3)
   end
   it 'should be made by TeX' do
     reader.info[:Creator].should eq('TeX')
   end
   it 'should have page 1 with given media box' do
-    reader.pages[0].attributes[:MediaBox].should eq([0, 0, 595.276, 841.89])
+    reader.pages[0].attributes[:MediaBox].should eq([0, 0, 297.638, 419.528])
   end
-  it 'should have 3 fonts on page 2' do
-    reader.pages[1].fonts.keys.size.should eq(3)
+  it 'should have 1 font on page 2' do
+    reader.pages[1].fonts.keys.size.should eq(1)
   end
-  it 'should start with a level 0 heading' do
-    reader.pages[0].text.should match('1Headingonlevel0\(chapter\).*')
+  it 'should contain 4 lines on page 3' do
+    reader.pages[2].text.should eq("augue.Crasacmagna.Crasnulla.Nulla\negestas.Curabituraleo.Quisqueegestas\nwisiegetnunc.Namfeugiatlacusvelest.\nCurabiturconsectetuer.\n3")
   end
 end
